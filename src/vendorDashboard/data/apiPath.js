b2eb504724c11065bfc7227edc4b17e1c4879f4e
kkarthik123 <159450549+kandula-karthik@users.noreply.github.com>
2025-05-23 09:33:35 +0530

    Update apiPath.js

--- src/vendorDashboard/data/apiPath.js
+++ src/vendorDashboard/data/apiPath.js
@@ -1,3 +1,3 @@
- export const API_URL = "http://localhost:4000"
+//export const API_URL = "http://localhost:4000"
 
-//export const API_URL = "https://backend-swgy.onrender.com/"
+export const API_URL = "https://backend-swgy.onrender.com/"
